cc9a77162b86ada3d9d8ae0ae518332afa9b3a24
Pablo Diaz <pdiaz2@uc.cl>
2018-06-08 02:06:12 -0400

    Working on MPC for thickener

---
+++ Control/Control_Espesador/mpcThickener_0706.m
@@ -0,0 +1,47 @@
+clear all;
+clc;
+close all;
+%% 
+N_y = 10;
+N_u = 3;
+Dt = 1;
+groupBy = 60; % This should be automatic
+tau_R = 10*groupBy;
+tau_C = 2*tau_R;
+stepInDV = false;
+imprint = false;
+controlClosedLoop = 1;
+startPlotTime = 1; %Wait for noise filter to stabilize
+dateMatFileStr = '0706';
+figurePath = 'figures\trialsTuningMPC\';
+%%
+%% Reference Values Struct
+wValuesStruct.delta = [0 5 -5 0];
+wValuesStruct.changeBool = logical([0 1 1 0]);
+wValuesStruct.shape = {'step','step','step','step'};
+wValuesStruct.timeToChange = [-1 floor(simTime/22) floor(simTime/2) -1];
+wValuesStruct.addNoiseBool = false; %always false for w
+%% Sensor Values Struct
+yValuesStruct.delta = [0 0 0 0];
+yValuesStruct.changeBool = logical([0 0 0 0]);
+yValuesStruct.shape = {'step','step','step','step'};
+yValuesStruct.timeToChange = [-1 -1 floor(simTime/22) -1];
+yValuesStruct.addNoiseBool = false;
+%%
+delayParametersFile = ['delayParameters_' dateMatFileStr '.mat'];
+fixedParametersFileName = ['mpc_fixed_parameters_' dateMatFileStr '.mat'];
+designParametersFileName = ['mpc_design_parameters_' dateMatFileStr '.mat'];
+parametersFileArray = {delayParametersFile,fixedParametersFileName,designParametersFileName};
+%% Generate parameters
+% Fixed parameters
+mpc_generate_fixed_parameters(dateMatFileStr,stepInDV,simTime);
+% Design parameters
+mpc_generate_design_parameters(dateMatFileStr,N_y,N_u);
+%% Filter Design
+bFilter = fir1(10,0.8,kaiser(11,6));
+% freqz(bFilter,1);
+bFilter = 1;
+%% Load Parameters
+for strFile = 1:3
+   load(parametersFileArray{strFile}); 
+end
--- Control/Control_Espesador/myMPCFunctions/mpc_generate_fixed_parameters.m
+++ Control/Control_Espesador/myMPCFunctions/mpc_generate_fixed_parameters.m
@@ -3,12 +3,15 @@ function mpc_generate_fixed_parameters(dateMatFileStr,stepInDV,simTime)
     matFileName = ['mpc_fixed_parameters_' dateMatFileStr '.mat'];
     delayParametersFile = ['delayParameters_' dateMatFileStr '.mat'];
     %% Initial Conditions
-    D0 = 35.7834;
-    U0 = [32.9816   46.2282   25.3416];
-    Y0 = [41.4120   28.7553   38.6037];
+    D0 = [325.1 0.3143 0.356];
+    U0 = [111 26]; 
+    Y0 = [21.02  0.737   2.126 0];
     numCV = length(Y0);
     numDV = length(D0);
     numMV = length(U0);
+    %% Noise
+    noisePower = [0.5 0.1 0.3 0.0001];
+    noiseSeed = [1231235 456345 93894 748563];
     %% DV Design
     if (stepInDV)
         D = [0,35.7834;floor(simTime/14),42.7834;3*floor(simTime/14),35.7834];
@@ -24,19 +27,21 @@ function mpc_generate_fixed_parameters(dateMatFileStr,stepInDV,simTime)
     maxDDelay = maxDelay(3);
     % Always print to check; MEX functions throw error anyway
     nTrees = ones(1,numCV)*100
-    nPredictors = [8 12 12]
+    nPredictors = [10 10 9 9]
     %% MPC Fixed Parameters
 
     % Constraints (Fixed by CIC2017)
     % On MV (1 pps => 5 pp5s)
-    deltaULowLim = -5;
-    deltaUHighLim = 5;
+    deltaULowLim = -3;
+    deltaUHighLim = 3;
 
     % Restriction only on y2: 0.95*Y0(2)<= y <= 1.05*Y0(2)
     yLowLims = 0*ones(numCV,1);
     yHighLims = 100*ones(numCV,1);
-    yLowLims(2,:) = 0.95*Y0(2);
-    yHighLims(2,:) = 1.05*Y0(2);
+    yLowLims(1,:) = 
+    yLowLims(2,:) = 0.97*Y0(2);
+    yHighLims(2,:) = 1.03*Y0(2);
+    
     
     % Constraints on U are free according to CIC2017
     uLowLims = 0*ones(numMV,1); 
