1c0e37087ba6662a2d530c4f09d84795ce968b03
Pablo Diaz <pdiaz2@uc.cl>
2018-06-15 02:30:14 -0400

    PatternSearch output function ready

--- Control/Control_Espesador/myMPCFunctions/mpc_output_patts_fcn.m
+++ Control/Control_Espesador/myMPCFunctions/mpc_output_patts_fcn.m
@@ -1,10 +1,10 @@
-function [ state, options, optchanged ] = mpc_output_ga_fcn(options, state, flag, ...
+function [stop, options, optchanged]  = mpc_output_patts_fcn(optimValues, options, flag, ...
                                                             qMatrix, betaCost, lambdaMatrix, wMatrix,...
                                                             yLims,uLims,...
                                                             yPastValues, uPastValues, dPastValues,...
                                                             nTrees, nPredictors,...
                                                             na, nb, nc, kappaControl,...
-                                                            plotProgress,championTolerance,...
+                                                            plotProgress, championTolerance,...
                                                             championCounterTolerance)
 %MPC_DISPLAY_OUTPUT_FCN Displays associated costs of objective function
 persistent bestMeanTrackingError bestMeanTerminalError bestMeanLimBreakError
@@ -12,42 +12,38 @@ persistent bestMaxTrackingError bestMaxTerminalError bestMaxLimBreakError
 persistent bestMinTrackingError bestMinTerminalError bestMinLimBreakError
 persistent bestTrackingCost bestTerminalCost bestLimBreakCost
 persistent h1 h2
-persistent champion championDistancePast championToleranceBreakCounter
-optchanged = false;
+% persistent champion championDistancePast championToleranceBreakCounter
+stop = false; % By default, don't stop.
 switch flag
     case 'init'
-%         bestScore = state.Best(end);
-%         ibest = find(state.Score == bestScore,1,'last');
-%         bestIndividual = state.Population(ibest,:);
-        champion = state.Population(1,:);
-        championDistancePast = 1e5;
-        championToleranceBreakCounter = 0;
+%         champion = optimValues.x;
+%         championDistancePast = 1e5;
+%         championToleranceBreakCounter = 0;
         if plotProgress
             h1 = figure;
             ax = gca;
-            ax.XLim = [0 (state.Generation+1)];
+            ax.XLim = [0 (optimValues.iteration+1)];
             movegui(h1,'west')
             h2 = figure;
             ax = gca;
-            ax.XLim = [0 (state.Generation+1)];
+            ax.XLim = [0 (optimValues.iteration+1)];
             movegui(h2,'east')
         else
             
         end
     case 'iter'
         [n,N_y] = size(wMatrix);
-        % Find the best objective function in current generation => Best(end)
-        bestScore = state.Best(end);
-        ibest = find(state.Score == bestScore,1,'last');
-        bestIndividual = state.Population(ibest,:);
-        decissionVars = numel(bestIndividual);
-        weightVector = ones(1,decissionVars);
-        championDistance = abs(bestIndividual-champion)*weightVector';
-        championSpread = championDistance-championDistancePast;
-        champion = bestIndividual;
-        if logical(plotProgress)
-            fprintf('Champion Distance: %4.6f\r\n',championDistance);
-        end
+        % Find the best objective function in current iteration
+        bestScore = optimValues.fval;
+        bestIndividual = optimValues.x;
+%         decissionVars = numel(bestIndividual);
+%         weightVector = ones(1,decissionVars);
+%         championDistance = abs(bestIndividual-champion)*weightVector';
+%         championSpread = championDistance-championDistancePast;
+%         champion = bestIndividual;
+%         if logical(plotProgress)
+%             fprintf('Champion Distance: %4.6f\r\n',optimValues.TolX);
+%         end
         [nPopulation,~] = size(bestIndividual);
         if logical(plotProgress)
             % Replication of yPastValues according to nPopulation
@@ -64,52 +60,52 @@ switch flag
             [ spTrackingError, terminalError, epsilonValues ] = mpc_horizon_error( yHat, wMatrix, yLims );
             if plotProgress == 1
                 % Means of errors
-                bestMeanTrackingError(1,state.Generation+1) = mean(abs(spTrackingError));
-                bestMeanTerminalError(1,state.Generation+1) = mean(abs(terminalError));
-                bestMeanLimBreakError(1,state.Generation+1) = mean(abs(epsilonValues));
+                bestMeanTrackingError(1,optimValues.iteration+1) = mean(abs(spTrackingError));
+                bestMeanTerminalError(1,optimValues.iteration+1) = mean(abs(terminalError));
+                bestMeanLimBreakError(1,optimValues.iteration+1) = mean(abs(epsilonValues));
                 % Max of errors
-                bestMaxTrackingError(1,state.Generation+1) = max(abs(spTrackingError));
-                bestMaxTerminalError(1,state.Generation+1) = max(abs(terminalError));
-                bestMaxLimBreakError(1,state.Generation+1) = max(abs(epsilonValues));
+                bestMaxTrackingError(1,optimValues.iteration+1) = max(abs(spTrackingError));
+                bestMaxTerminalError(1,optimValues.iteration+1) = max(abs(terminalError));
+                bestMaxLimBreakError(1,optimValues.iteration+1) = max(abs(epsilonValues));
                 % Min of errors
-                bestMinTrackingError(1,state.Generation+1) = min(abs(spTrackingError));
-                bestMinTerminalError(1,state.Generation+1) = min(abs(terminalError));
-                bestMinLimBreakError(1,state.Generation+1) = min(abs(epsilonValues));
+                bestMinTrackingError(1,optimValues.iteration+1) = min(abs(spTrackingError));
+                bestMinTerminalError(1,optimValues.iteration+1) = min(abs(terminalError));
+                bestMinLimBreakError(1,optimValues.iteration+1) = min(abs(epsilonValues));
             elseif plotProgress == 2
                 matrixSpTrackingError = reshape(spTrackingError,n,N_y-1);
                 matrixTerminalError = reshape(terminalError,n,1);
                 matrixEpsilonValues = reshape(epsilonValues,n,N_y);
                 % Means of errors
-                bestMeanTrackingError(:,state.Generation+1) = mean(abs(matrixSpTrackingError),2);
-                bestMeanTerminalError(:,state.Generation+1) = mean(abs(matrixTerminalError),2);
-                bestMeanLimBreakError(:,state.Generation+1) = mean(abs(matrixEpsilonValues),2);
+                bestMeanTrackingError(:,optimValues.iteration+1) = mean(abs(matrixSpTrackingError),2);
+                bestMeanTerminalError(:,optimValues.iteration+1) = mean(abs(matrixTerminalError),2);
+                bestMeanLimBreakError(:,optimValues.iteration+1) = mean(abs(matrixEpsilonValues),2);
                 % Max of errors
-                bestMaxTrackingError(:,state.Generation+1) = max(abs(matrixSpTrackingError),[],2);
-                bestMaxTerminalError(:,state.Generation+1) = max(abs(matrixTerminalError),[],2);
-                bestMaxLimBreakError(:,state.Generation+1) = max(abs(matrixEpsilonValues),[],2);
+                bestMaxTrackingError(:,optimValues.iteration+1) = max(abs(matrixSpTrackingError),[],2);
+                bestMaxTerminalError(:,optimValues.iteration+1) = max(abs(matrixTerminalError),[],2);
+                bestMaxLimBreakError(:,optimValues.iteration+1) = max(abs(matrixEpsilonValues),[],2);
                 % Min of errors
-                bestMinTrackingError(:,state.Generation+1) = min(abs(matrixSpTrackingError),[],2);
-                bestMinTerminalError(:,state.Generation+1) = min(abs(matrixTerminalError),[],2);
-                bestMinLimBreakError(:,state.Generation+1) = min(abs(matrixEpsilonValues),[],2);
+                bestMinTrackingError(:,optimValues.iteration+1) = min(abs(matrixSpTrackingError),[],2);
+                bestMinTerminalError(:,optimValues.iteration+1) = min(abs(matrixTerminalError),[],2);
+                bestMinLimBreakError(:,optimValues.iteration+1) = min(abs(matrixEpsilonValues),[],2);
             end
             % Plot
-            if rem(state.Generation,10) == 0
+            if rem(optimValues.iteration,10) == 0
                 figure(h1)
                 ax = gca;
-                ax.XLim = [0 (state.Generation+1)];
+                ax.XLim = [0 (optimValues.iteration+1)];
                 if plotProgress == 1
-                    plot(0:state.Generation,bestMeanTrackingError,'r+')
+                    plot(0:optimValues.iteration,bestMeanTrackingError,'r+')
                     hold on
-                    plot(0:state.Generation,bestMeanTerminalError,'b*')
-                    plot(0:state.Generation,bestMeanLimBreakError,'go')
+                    plot(0:optimValues.iteration,bestMeanTerminalError,'b*')
+                    plot(0:optimValues.iteration,bestMeanLimBreakError,'go')
 
-                    plot(0:state.Generation,bestMaxTrackingError,'r--')
-                    plot(0:state.Generation,bestMaxTerminalError,'b--')
-                    plot(0:state.Generation,bestMaxLimBreakError,'g--')
+                    plot(0:optimValues.iteration,bestMaxTrackingError,'r--')
+                    plot(0:optimValues.iteration,bestMaxTerminalError,'b--')
+                    plot(0:optimValues.iteration,bestMaxLimBreakError,'g--')
 
-                    plot(0:state.Generation,bestMinTrackingError,'r--')
-                    plot(0:state.Generation,bestMinTerminalError,'b--')
-                    plot(0:state.Generation,bestMinLimBreakError,'g--')
+                    plot(0:optimValues.iteration,bestMinTrackingError,'r--')
+                    plot(0:optimValues.iteration,bestMinTerminalError,'b--')
+                    plot(0:optimValues.iteration,bestMinLimBreakError,'g--')
                     hold off
                     grid on
                     legend({'Mean Track Error','Mean Terminal Error','Mean LimBreak Error'},'Location','southwest')
@@ -118,12 +114,12 @@ switch flag
                     markerType = {'*','o','s','.'};
                     colorType = {'b','r','g'};
                     for cv = 1:n
-                        plot(0:state.Generation,bestMaxTerminalError(cv,:),...
+                        plot(0:optimValues.iteration,bestMaxTerminalError(cv,:),...
                             'Marker',markerType{cv},'Color',colorType{1})
                         hold on
-                        plot(0:state.Generation,bestMaxTrackingError(cv,:),...
+                        plot(0:optimValues.iteration,bestMaxTrackingError(cv,:),...
                             'Marker',markerType{cv},'Color',colorType{2})
-                        plot(0:state.Generation,bestMaxLimBreakError(cv,:),...
+                        plot(0:optimValues.iteration,bestMaxLimBreakError(cv,:),...
                             'Marker',markerType{cv},'Color',colorType{3})
                         legendArray{end+1} = ['MaxTerm_' num2str(cv)];
                         legendArray{end+1} = ['MaxTrack_' num2str(cv)];
@@ -137,28 +133,30 @@ switch flag
             % Costs of errors
             [ spTrackingCost, terminalCost, limBreakCost ] = mpc_horizon_cost( spTrackingError, terminalError, epsilonValues, ...
                                                                             qMatrix,betaCost,lambdaMatrix);
-            bestTrackingCost(1,state.Generation+1) = 1/2*spTrackingCost;
-            bestTerminalCost(1,state.Generation+1) = 1/2*terminalCost;
-            bestLimBreakCost(1,state.Generation+1) = 1/2*limBreakCost;
+            bestTrackingCost(1,optimValues.iteration+1) = 1/2*spTrackingCost;
+            bestTerminalCost(1,optimValues.iteration+1) = 1/2*terminalCost;
+            bestLimBreakCost(1,optimValues.iteration+1) = 1/2*limBreakCost;
 
             % Plot
-            if rem(state.Generation,10) == 0
+            if rem(optimValues.iteration,10) == 0
                 figure(h2)
                 ax = gca;
-                ax.XLim = [0 (state.Generation+1)];
-                plot(0:state.Generation,bestTrackingCost,'r*')
+                ax.XLim = [0 (optimValues.iteration+1)];
+                plot(0:optimValues.iteration,bestTrackingCost,'r*')
                 hold on
-                plot(0:state.Generation,bestTerminalCost,'b.')
-                plot(0:state.Generation,bestLimBreakCost,'go')
+                plot(0:optimValues.iteration,bestTerminalCost,'b.')
+                plot(0:optimValues.iteration,bestLimBreakCost,'go')
                 hold off
                 grid on
                 legend({'Track Cost','Terminal Cost','LimBreak Cost'},'Location','northeast')
-                fprintf('Generation %d\r\n',state.Generation);
-                fprintf('Last Improvement Generation %d\r\n',state.LastImprovement)
+                fprintf('Generation %d\r\n',optimValues.iteration);
+                fprintf('Method used %s\r\n',optimValues.method)
+                fprintf('Mesh size %4.4f',optimValues.meshsize);
                 fprintf('Delta U(0) is %4.4f\r\n',bestIndividual(1:2))
                 fprintf('F.O.Value Now is %4.8f\r\n',bestScore)
-                fprintf('Total Function Evaluations %d\r\n',state.FunEval)
-                fprintf('Champion Distance Counter is %d\r\n',championToleranceBreakCounter);
+                fprintf('Total Function Evaluations %d\r\n',optimValues.funccount)
+                fprintf('Champion Distance: %4.6f\r\n',optimValues.TolX);
+                fprintf('Change inf F.O : %4.6f\r\n',optimValues.TolX);
                 pause()
             end
         else
@@ -167,11 +165,12 @@ switch flag
 %             championToleranceBreakCounter = championToleranceBreakCounter+1;   
 %         end
         if (championDistance) < championTolerance
-            championToleranceBreakCounter = championToleranceBreakCounter+1;   
+            championToleranceBreakCounter = championToleranceBreakCounter+1;
+            
         end
 
         if championToleranceBreakCounter > championCounterTolerance
-           state.StopFlag = 'y'; 
+           stop = true;
         end
         championDistancePast = championDistance;
     case 'done'
